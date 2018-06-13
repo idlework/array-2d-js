1a45132d8379873378edfa34a3c29091bacea9b7
Johan Haneveld <johan.haneveld@gmail.com>
2018-06-14 00:06:05 +0200

    add tests and fixed Array2D where failing

--- src/Array2D.js
+++ src/Array2D.js
@@ -1,9 +1,9 @@
-export default class Array2D {
+module.exports = class Array2D {
   /**
    * constructor - intitialize Array2D class.
    *
-   * @param  {number} width  number corresponding with the width(column count) of the 2d array.
-   * @param  {number} height number corresponding with the height(row count) of the 2d array.
+   * @param {number} width  number corresponding with the width(column count) of the 2d array.
+   * @param {number} height number corresponding with the height(row count) of the 2d array.
    */
   constructor (width, height) {
     this._width = width
@@ -16,9 +16,60 @@ export default class Array2D {
   }
 
   /**
+   * getColumn - Return the column of the given position.
+   *
+   * @param {number} position Position for the column.
+   * @return {array} Returns the column.
+   */
+  getColumn (position) {
+    return this._array2D[position]
+  }
+
+  /**
+   * addColumn - Add the column at the given position.
+   *
+   * @param {number} position Position for the column.
+   */
+  addColumn (position) {
+    this._array2D.splice(position, 0, Array(this.height))
+  }
+
+  /**
+   * removeColumn - Remove the column at the given position.
+   *
+   * @param {number} position Position for the column.
+   */
+  removeColumn (position) {
+    this._array2D.splice(position, 1)
+  }
+
+  /**
+   * fillColumn - Executes the provided function once for each array element for column at given position.
+   *
+   * @param {number} position Position for the column.
+   * @param {function} f Fill function for the column.
+   */
+  fillColumn (position, f) {
+    // @TODO: find out why you can't use forEach() on an array that contains empty arrays, use es3 style for now.
+    for (let i = 0; i < this.height; i++) {
+      this.fillPosition(position, i, f(position, i))
+    }
+  }
+
+  /**
+   * clearColumn - Clear the column at the given position.
+   *
+   * @param {number} position Position for the column.
+   */
+  clearColumn (position) {
+    const column = this.getColumn(position)
+    column.forEach((item, index) => this.clearPosition(position, index))
+  }
+
+  /**
    * getRow - Return the row of the given position.
    *
-   * @param  {number} position Position for the row.
+   * @param {number} position Position for the row.
    * @return {array} Returns the row.
    */
   getRow (position) {
@@ -28,26 +79,26 @@ export default class Array2D {
   /**
    * addRow - Add the row at the given position.
    *
-   * @param  {number} position Position for the row.
+   * @param {number} position Position for the row.
    */
   addRow (position) {
-    this._array2D.forEach(column => column.splice(position, 1, undefined))
+    this._array2D.forEach(column => column.splice(position, 0, undefined))
   }
 
   /**
    * removeRow - Remove the row at the given position.
    *
-   * @param  {number} position Position for the row.
+   * @param {number} position Position for the row.
    */
   removeRow (position) {
-    this._array2D.forEach(column => column.slice(position, 1))
+    this._array2D.forEach(column => column.splice(position, 1))
   }
 
   /**
    * fillRow - Executes the provided function once for each array element for row at given position.
    *
-   * @param  {number} position Position for the row.
-   * @param  {function} f Fill function for the row.
+   * @param {number} position Position for the row.
+   * @param {function} f Fill function for the row.
    */
   fillRow (position, f) {
     const row = this.getRow(position)
@@ -65,59 +116,10 @@ export default class Array2D {
   }
 
   /**
-   * getColumn - Return the column of the given position.
-   *
-   * @param  {number} position Position for the column.
-   * @return {array} Returns the column.
-   */
-  getColumn (position) {
-    return this._array2D[position]
-  }
-
-  /**
-   * addColumn - Add the column at the given position.
-   *
-   * @param  {number} position Position for the column.
-   */
-  addColumn (position) {
-    this._array2D.splice(position, 1, Array(this.height))
-  }
-
-  /**
-   * removeColumn - Remove the column at the given position.
-   *
-   * @param  {number} position Position for the column.
-   */
-  removeColumn (position) {
-    this._array2D.slice(position, 1)
-  }
-
-  /**
-   * fillColumn - Executes the provided function once for each array element for column at given position.
-   *
-   * @param  {number} position Position for the column.
-   * @param  {function} f Fill function for the column.
-   */
-  fillColumn (position, f) {
-    const column = this.getColumn(position)
-    column.forEach((item, index) => this.fillPosition(position, index, f(position, index)))
-  }
-
-  /**
-   * clearColumn - Clear the column at the given position.
-   *
-   * @param  {number} position Position for the column.
-   */
-  clearColumn (position) {
-    const column = this.getColumn(position)
-    column.forEach((item, index) => this.clearPosition(position, index))
-  }
-
-  /**
    * getPosition - Return the item of the given position.
    *
-   * @param  {number} x x(column) position for in the 2d array
-   * @param  {number} y y(row) position for in the 2d array
+   * @param {number} x x(column) position for in the 2d array
+   * @param {number} y y(row) position for in the 2d array
    * @return {any} returns the item
    */
   getPosition (x, y) {
@@ -127,9 +129,9 @@ export default class Array2D {
   /**
    * fillPosition - Fill position with item
    *
-   * @param  {number} x x(column) position for in the 2d array
-   * @param  {number} y y(row) position for in the 2d array
-   * @param  {any} item item for in the 2d array
+   * @param {number} x x(column) position for in the 2d array
+   * @param {number} y y(row) position for in the 2d array
+   * @param {any} item item for in the 2d array
    */
   fillPosition (x, y, item) {
     this._array2D[x][y] = item
@@ -137,8 +139,8 @@ export default class Array2D {
 
   /**
    *
-   * @param  {type} x x(column) position for in the 2d array
-   * @param  {type} y y y(row) position for in the 2d array
+   * @param {type} x x(column) position for in the 2d array
+   * @param {type} y y y(row) position for in the 2d array
    */
   clearPosition (x, y) {
     this._array2D[x][y] = undefined
@@ -148,7 +150,12 @@ export default class Array2D {
    * forEach - Executes a provided function once for each 2d array element.
    */
   forEach (f) {
-    this.map.forEach((column, x) => column.forEach((item, y) => f(item, { x, y })))
+    // @TODO: find out why you can't use forEach() on an array that contains empty arrays, use es3 style for now.
+    for (let y = 0; y < this.height; y++) {
+      for (let x = 0; x < this.width; x++) {
+        f(this.getPosition(x, y), { x, y })
+      }
+    }
   }
 
   /**
@@ -166,7 +173,7 @@ export default class Array2D {
    * @return {number} return the width of the 2d array
    */
   get width () {
-    return this.array2d.length
+    return this.array2D.length
   }
 
   /**
@@ -175,6 +182,6 @@ export default class Array2D {
    * @return {number} return the height of the 2d array
    */
   get height () {
-    return this.array2d[0].length
+    return this.array2D[0].length
   }
 }
---
+++ test/Array2D.js
@@ -0,0 +1,136 @@
+import Array2D from './../src/Array2D'
+import test from 'ava'
+
+test('create empty 2d array', t => {
+  const array2D = new Array2D()
+  t.truthy(array2D)
+})
+
+test('get 2d array', t => {
+  const array2D = new Array2D()
+  t.truthy(array2D.array2D)
+})
+
+test('create 10 x 10 2d array and check width, height', t => {
+  const array2D = new Array2D(10, 10)
+  t.truthy(array2D)
+  t.is(array2D.width, 10)
+  t.is(array2D.height, 10)
+})
+
+test('get empty postion at 5, 5', t => {
+  const array2D = new Array2D(10, 10)
+  const item = array2D.getPosition(5, 5)
+  t.falsy(item)
+})
+
+test('fill postion at 5, 5 with x', t => {
+  const array2D = new Array2D(10, 10)
+  array2D.fillPosition(5, 5, 'x')
+  const item = array2D.getPosition(5, 5)
+  t.is(item, 'x')
+})
+
+test('clear postion at 5, 5', t => {
+  const array2D = new Array2D(10, 10)
+  array2D.fillPosition(5, 5, 'x')
+  let item = array2D.getPosition(5, 5)
+  t.is(item, 'x')
+  array2D.clearPosition(5, 5)
+  item = array2D.getPosition(5, 5)
+  t.falsy(item)
+})
+
+test('forEach loop over every item', t => {
+  const array2D = new Array2D(10, 10)
+  let xPos = 0
+  let yPos = 0
+  array2D.forEach((item, { x, y }) => {
+    t.falsy(item)
+    t.is(x, xPos)
+    t.is(y, yPos)
+    xPos += 1
+    xPos %= 10
+    xPos === 0 && (yPos += 1)
+  })
+})
+
+test('get empty column at position 2', t => {
+  const array2D = new Array2D(10, 10)
+  const column = array2D.getColumn(2)
+  t.truthy(column)
+  t.is(column.length, 10)
+})
+
+test('add empty column at position 9', t => {
+  const array2D = new Array2D(10, 10)
+  array2D.addColumn(9)
+  t.is(array2D.width, 11)
+  const column = array2D.getColumn(9)
+  t.truthy(column)
+  t.is(column.length, 10)
+  t.falsy(column[2])
+})
+
+test('remove column at position 9', t => {
+  const array2D = new Array2D(10, 10)
+  array2D.removeColumn(9)
+  t.is(array2D.width, 9)
+})
+
+test('fill column at position 5 with x', t => {
+  const array2D = new Array2D(10, 10)
+  array2D.fillColumn(5, _ => 'x')
+  const column = array2D.getColumn(5)
+  column.forEach(item => (t.is(item, 'x')))
+})
+
+test('clear column at position 5', t => {
+  const array2D = new Array2D(10, 10)
+  array2D.fillColumn(5, _ => 'x')
+  let column = array2D.getColumn(5)
+  column.forEach(item => (t.is(item, 'x')))
+  array2D.clearColumn(5)
+  column = array2D.getColumn(5)
+  column.forEach(item => (t.falsy(item)))
+})
+
+test('get empty row at position 2', t => {
+  const array2D = new Array2D(10, 10)
+  const row = array2D.getRow(2)
+  t.truthy(row)
+  t.is(row.length, 10)
+})
+
+test('add empty row at position 9', t => {
+  const array2D = new Array2D(10, 10)
+  array2D.addRow(9)
+  t.is(array2D.height, 11)
+  const row = array2D.getRow(9)
+  t.truthy(row)
+  t.is(row.length, 10)
+  t.falsy(row[2])
+})
+
+test('remove row at position 9', t => {
+  const array2D = new Array2D(10, 10)
+  array2D.removeRow(9)
+  t.is(array2D.height, 9)
+})
+
+test('fill row at position 5 with x', t => {
+  const array2D = new Array2D(10, 10)
+  array2D.fillRow(5, _ => 'x')
+  const row = array2D.getRow(5)
+  row.forEach(item => (t.is(item, 'x')))
+})
+
+test('clear row at position 5', t => {
+  const array2D = new Array2D(10, 10)
+  array2D.fillRow(5, _ => 'x')
+  let row = array2D.getRow(5)
+  row.forEach(item => (t.is(item, 'x')))
+  array2D.clearRow(5)
+  row = array2D.getRow(5)
+  row.forEach(item => (t.falsy(item)))
+})
